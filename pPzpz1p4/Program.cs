07dd7ea53181e8b4c409e79d2e46b0fff934817e
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 16:22:41 +0100

    Add files via upload

---
+++ pPzpz1p4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pPzpz1p4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = "";
+            for (int i = 1; i < n; i++)
+            {
+                s += i;
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
